24eee5f19da9a20fca0e389b23df85a9ef876115
irolram <ivanr9016@gmail.com>
2025-11-06 19:43:05 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Ej2_Tema3_IvanRoldanRamos',
+  webDir: 'www'
+};
+
+export default config;
